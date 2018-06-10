2f27c9ea46d302217d8e06dffe8acfd2ed4bbfbd
Felipe Natan <39018332+fenatan@users.noreply.github.com>
2018-06-10 01:48:26 -0300

    Create 1178.cpp

---
+++ 1178.cpp
@@ -0,0 +1,12 @@
+include <stdio.h>
+
+int main(){
+    double t;
+    scanf("%lf", &t);
+
+    for(int i =0; i < 100; i++){
+        printf("N[%d] = %.4lf\n", i,t);
+        t = t/2;
+    }
+    return 0;
+}
